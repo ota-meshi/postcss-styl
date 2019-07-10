2ec0a77b53d763ca399b1bdda6d32229a35df2b7
yosuke ota <otameshiyo23@gmail.com>
2019-07-11 01:28:23 +0900

    wip

--- tests/index.js
+++ tests/index.js
@@ -5,28 +5,19 @@ const postcss = require("postcss")
 
 const stylusPostcss = require("../")
 
-// it("try", () => {
-//     const result = postcss().process(
-//         //
-//         `
+it("try", () => {
+    const result = postcss().process(
+        //
+        `
 
-// @keyframes slidein {
-//     from {
-//         margin-left: 100%;
-//         width: 300%;
-//     }
-
-//     to {
-//         margin-left: 0%;
-//         width: 100%;
-//     }
-// }
-// `,
-//         { syntax: stylusPostcss }
-//     ).root
-//     assert.strictEqual(typeof result, "object")
-//     assert.strictEqual(result.toString(), "")
-// })
+    {selector('.a', '.b', '.c, .d')}
+        color: red
+`,
+        { syntax: stylusPostcss }
+    ).root
+    assert.strictEqual(typeof result, "object")
+    assert.strictEqual(result.toString(), "")
+})
 
 it("parse stylus as postcss syntax", () => {
     const result = postcss().process(
