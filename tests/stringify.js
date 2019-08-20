c5dc8d6bf25ad7e7c451cb9137f3cdca3d6ffdfe
Yosuke Ota <otameshiyo23@gmail.com>
2019-08-20 22:37:48 +0900

    Stylus Version Up (#13)

--- tests/stringify.js
+++ tests/stringify.js
@@ -84,20 +84,6 @@ describe("stringify", () => {
                 }
             })
 
-            try {
-                parse(transformRoot.toString(stringify), {
-                    from: fixture.files["transform-omits.styl"],
-                })
-            } catch (_e) {
-                // cannot parse?
-                transformRoot.walkDecls(node => {
-                    if (node.raws.stylusBetween) {
-                        transformed = true
-                        delete node.raws.stylusBetween
-                    }
-                })
-            }
-
             if (!transformed) {
                 deleteFixture(fixture.files["transform-omits.styl"])
             }
